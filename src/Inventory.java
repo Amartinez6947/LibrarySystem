7c5e9b7d4c44e354d6a2e71bb17ce8285e6c7332
Amartinez6947 <alexa.martinez@csuglobal.edu>
2026-05-08 12:12:47 -0600

    Update Inventory.java

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,4 +1,81 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
-public class Inventory {
-}
+public class Inventory
+{
+    private ArrayList<Book> books;
+    private ArrayList<Book> borrowedBooks;
+
+    public Inventory()
+    {
+        books = new ArrayList<>();
+        borrowedBooks = new ArrayList<>();
+    }
+
+    public void addBook(Book book)
+    {
+        books.add(book);
+    }
+
+    public boolean borrowBook(int id)
+    {
+        for (Book book : books)
+        {
+            if (book.getId() == id)
+            {
+                borrowedBooks.add(book);
+                books.remove(book);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean returnBook(int id)
+    {
+        for (Book book : borrowedBooks)
+        {
+            if (book.getId() == id)
+            {
+                books.add(book);
+                borrowedBooks.remove(book);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void printAll()
+    {
+        if (books.isEmpty())
+        {
+            System.out.println("No books in library.");
+        }
+        else
+        {
+            for (Book book : books)
+            {
+                book.printBookInfo();
+            }
+        }
+    }
+
+    public void searchByTitle(String title)
+    {
+        boolean found = false;
+
+        for (Book book : books)
+        {
+            if (book.getTitle().toLowerCase().contains(title.toLowerCase()))
+            {
+                book.printBookInfo();
+                found = true;
+            }
+        }
+
+        if (!found)
+        {
+            System.out.println("No matching book found.");
+        }
+    }
